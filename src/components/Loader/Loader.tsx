bea045c5c441bf99fd25e59ac474eda710321555
Marcelo <marceloalarconbarrenechea@gmail.com>
2023-09-19 15:37:34 -0400

    update loading bar size

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -2,7 +2,7 @@ import style from './Loader.module.css'
 
 const Loader: React.FC = () => {
     return (
-        <div className={`${style.loaderContainer} w-4/5 rounded-xl`}>
+        <div className={`${style.loaderContainer} w-4/5 max-w-xl rounded-xl`}>
             <div className={`${style.loader}`}>
                 <div className={`${style.loaderBar}`}></div>
             </div>
