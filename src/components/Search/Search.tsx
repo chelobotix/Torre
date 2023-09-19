32d1672bcb8788e0647a8f7d406c1e05acc30213
Marcelo <marceloalarconbarrenechea@gmail.com>
2023-09-19 11:19:22 -0400

    Add LocalStorage

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -5,6 +5,12 @@ import { type IUser } from '../../interfaces/userInterface'
 import { UserCard } from '../UserCard/UserCard'
 import { SearchResult } from '../SearchResult/SearchResult'
 import { Loader } from '../Loader/Loader'
+import { HandleLocalStorage } from '../../helper/LocalStorage'
+import { QueryHistory } from '../QueryHistory/QueryHistory'
+import { getDate } from '../../helper/getDate'
+
+const userLocalStorage = new HandleLocalStorage('users')
+const aux = userLocalStorage.getData()
 
 interface ISearch {
     text: string
@@ -21,6 +27,9 @@ const initialState: ISearch = {
     result: [],
     history: [],
 }
+if (Array.isArray(aux)) {
+    initialState.history = [...aux]
+}
 
 const Search: React.FC = () => {
     const [search, setSearch] = useState<ISearch>(initialState)
@@ -32,12 +41,13 @@ const Search: React.FC = () => {
 
     const handleSearchKeyDown = (e: React.KeyboardEvent<HTMLInputElement>): void => {
         if (e.key === 'Enter') {
-            setSearch((prev) => ({ ...prev, result: search.users, history: [...search.history, search.text] }))
-            localStorage.setItem('users', JSON.stringify(search.history))
+            const query = `${search.text} on ${getDate()}`
+            setSearch((prev) => ({ ...prev, result: search.users, history: [...search.history, query] }))
         }
     }
 
     useEffect(() => {
+        userLocalStorage.setData(search.history)
         if (firstRenderRef.current) {
             firstRenderRef.current = false
         } else {
@@ -48,11 +58,18 @@ const Search: React.FC = () => {
             }
             void userFetcher('https://torre.ai/api/entities/_searchStream', queryObj, setSearch)
         }
-    }, [search.text])
+    }, [search.text, search.history])
     return (
         <div>
             <input type="text" value={search.text} onChange={handleSearchChange} onKeyDown={handleSearchKeyDown} />
             {search.isLoading && <Loader />}
+            <div>
+                <ul>
+                    {search.history.map((query) => (
+                        <QueryHistory key={uuidv4()} query={query} />
+                    ))}
+                </ul>
+            </div>
             <div>
                 <ul>
                     {search.users !== null
